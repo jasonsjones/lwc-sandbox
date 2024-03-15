c55a579c3ab5af352be61bb6636257829a021651
Jason Jones <jsjones96@gmail.com>
2024-03-15 14:41:46 -0700

    chore: format code

--- packages/app/rollup.config.js
+++ packages/app/rollup.config.js
@@ -1,16 +1,16 @@
-import lwc from "@lwc/rollup-plugin";
-import replace from "@rollup/plugin-replace";
+import lwc from '@lwc/rollup-plugin';
+import replace from '@rollup/plugin-replace';
 
 export default {
-  input: "src/main.js",
-  output: {
-    file: "dist/main.js",
-    format: "esm",
-  },
-  plugins: [
-    replace({
-      "process.env.NODE_ENV": JSON.stringify("development"),
-    }),
-    lwc(),
-  ],
+    input: 'src/main.js',
+    output: {
+        file: 'dist/main.js',
+        format: 'esm'
+    },
+    plugins: [
+        replace({
+            'process.env.NODE_ENV': JSON.stringify('development')
+        }),
+        lwc()
+    ]
 };
--- packages/app/src/server.js
+++ packages/app/src/server.js
@@ -1,6 +1,6 @@
-import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,12 +8,12 @@ const port = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, "../dist")));
+app.use(express.static(path.join(__dirname, '../dist')));
 
 // sendFile will go here
-app.get("/", function (_req, res) {
-  res.sendFile(path.join(__dirname, "/index.html"));
+app.get('/', function (_req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.listen(port);
-console.log("Server started at http://localhost:" + port);
+console.log('Server started at http://localhost:' + port);
